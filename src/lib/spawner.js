2fa66e8b44023431151dd068d3ebf556028c3b44
Victor Cheong <vche036@gmail.com>
2023-09-23 19:03:46 -0400

    Add nouns

--- src/lib/spawner.js
+++ src/lib/spawner.js
@@ -11,6 +11,7 @@ export const preload = async () => {
   models.hotdog = await loadMesh(glbs.hotdog);
   models.cheems = await loadMesh(glbs.cheems);
   models.doge = await loadMesh(glbs.doge);
+  models.nouns = await loadMesh(glbs.nouns);
 };
 
 export let loadMesh = async (url) => {
@@ -51,6 +52,15 @@ export const spawn = (thing) => {
     let doge = models.doge.clone();
     doge.rotation.set(0, Math.random() * 10, 0);
     obj.mesh = doge;
+    obj.sound = sounds.laser;
+    obj.mass = 1;
+    obj.rDamp = 0.01 + Math.random() * 0.03;
+  }
+
+  if (thing === "nouns") {
+    let nouns = models.nouns.clone();
+    nouns.rotation.set(0, Math.random() * 10, 0);
+    obj.mesh = nouns;
     obj.sound = sounds.laser;
     obj.mass = 1;
     obj.rDamp = 0.01 + Math.random() * 0.03;
