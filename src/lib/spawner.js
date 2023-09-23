dd8385d54cd544486c867c371d7397fe8dc0d22f
Victor Cheong <vche036@gmail.com>
2023-09-23 18:35:04 -0400

    Add doge

--- src/lib/spawner.js
+++ src/lib/spawner.js
@@ -10,6 +10,7 @@ export let glbSrc = glbs;
 export const preload = async () => {
   models.hotdog = await loadMesh(glbs.hotdog);
   models.cheems = await loadMesh(glbs.cheems);
+  models.doge = await loadMesh(glbs.doge);
 };
 
 export let loadMesh = async (url) => {
@@ -41,6 +42,15 @@ export const spawn = (thing) => {
     let cheems = models.cheems.clone();
     cheems.rotation.set(0, Math.random() * 10, 0);
     obj.mesh = cheems;
+    obj.sound = sounds.laser;
+    obj.mass = 1;
+    obj.rDamp = 0.01 + Math.random() * 0.03;
+  }
+
+  if (thing === "doge") {
+    let doge = models.doge.clone();
+    doge.rotation.set(0, Math.random() * 10, 0);
+    obj.mesh = doge;
     obj.sound = sounds.laser;
     obj.mass = 1;
     obj.rDamp = 0.01 + Math.random() * 0.03;
