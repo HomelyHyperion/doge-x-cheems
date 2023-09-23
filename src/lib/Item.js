d945e3878a7c789077a5f464f4bc99323b1f944b
Victor Cheong <vche036@gmail.com>
2023-09-23 15:31:38 -0400

    Add lib files

---
+++ src/lib/Item.js
@@ -0,0 +1,74 @@
+/*
+    Item class    
+*/
+
+import * as THREE from "three";
+import { Howl } from "howler";
+import { spawn } from "./spawner.js";
+
+export class Item {
+  constructor(pos, height, thing) {
+    const obj = spawn(thing);
+
+    this.mesh = obj.mesh;
+    this.mass = obj.mass;
+    this.sound = new Howl({
+      src: [obj.sound],
+    });
+
+    this.mesh.castShadow = true;
+    this.mesh.position.copy(pos);
+
+    this.mesh.position.y = height;
+    this.bottom = pos.y;
+    this.rotationLimit = Math.PI * 4;
+    this.hasPlayed = true;
+
+    // position
+    this.vel = new THREE.Vector3();
+    this.acc = new THREE.Vector3();
+
+    // rotation
+    this.dRot = new THREE.Euler(
+      Math.random() * this.rotationLimit,
+      Math.random() * this.rotationLimit,
+      Math.random() * this.rotationLimit
+    );
+    this.rDamp = obj.rDamp;
+  }
+
+  applyForce(force) {
+    // newton
+    let f = force.clone();
+    f.divideScalar(this.mass);
+    this.acc.add(f);
+  }
+
+  update() {
+    // position
+    this.vel.add(this.acc);
+    this.mesh.position.add(this.vel);
+    this.acc.multiplyScalar(0);
+
+    // rotations
+    const rx = (this.dRot.x - this.mesh.rotation.x) * this.rDamp;
+    const ry = (this.dRot.y - this.mesh.rotation.y) * this.rDamp;
+    const rz = (this.dRot.z - this.mesh.rotation.z) * this.rDamp;
+    this.mesh.rotation.set(
+      (this.mesh.rotation.x += rx),
+      (this.mesh.rotation.y += ry),
+      (this.mesh.rotation.z += rz)
+    );
+
+    // land
+    if (this.mesh.position.y < this.bottom + 0.2) {
+      if (!this.hasPlayed) this.sound.play();
+      this.hasPlayed = true;
+    }
+
+    // bottom
+    if (this.mesh.position.y < this.bottom) {
+      this.mesh.position.y = this.bottom;
+    }
+  }
+}
---
+++ src/lib/spawner.js
@@ -0,0 +1,50 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import sounds from "../media/sounds/*.mp3";
+import glbs from "../media/3d/*.glb";
+
+let models = {};
+
+export let glbSrc = glbs;
+
+export const preload = async () => {
+  models.hotdog = await loadMesh(glbs.hotdog);
+  models.cheems = await loadMesh(glbs.cheems);
+};
+
+export let loadMesh = async (url) => {
+  const gltf = await modelLoader(url);
+  return gltf.scene.children[0];
+};
+
+const loader = new GLTFLoader();
+
+const modelLoader = (url) => {
+  return new Promise((resolve, reject) => {
+    loader.load(url, (data) => resolve(data), null, reject);
+  });
+};
+
+export const spawn = (thing) => {
+  let obj = {};
+
+  if (thing === "hotdog") {
+    let hotdog = models.hotdog.clone();
+    hotdog.rotation.set(0, Math.random() * 10, 0);
+    obj.mesh = hotdog;
+    obj.sound = sounds.squelch;
+    obj.mass = 2;
+    obj.rDamp = 0.01 + Math.random() * 0.03;
+  }
+
+  if (thing === "cheems") {
+    let cheems = models.cheems.clone();
+    cheems.rotation.set(0, Math.random() * 10, 0);
+    obj.mesh = cheems;
+    obj.sound = sounds.laser;
+    obj.mass = 1;
+    obj.rDamp = 0.01 + Math.random() * 0.03;
+  }
+
+  return obj;
+};
